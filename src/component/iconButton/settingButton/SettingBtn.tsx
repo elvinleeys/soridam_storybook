9f21041fe33add4c2c347b567336a22daf2d5ca2
Elvin-lee <herjun802@gmail.com>
2025-08-11 15:42:17 +0900

    [Feat] : Setting icon Button 컴포넌트 및 스토리 완성

---
+++ src/component/iconButton/settingButton/SettingBtn.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+
+const sizeStyles = {
+  sm: "w-[1.125rem] h-[1.125rem]",
+  md: "w-[1.5rem] h-[1.5rem]",
+  lg: "w-[2rem] h-[2rem]",
+}
+
+export type SettingBtnSize = keyof typeof sizeStyles;
+
+export interface SettingBtnProps {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  size?: SettingBtnSize; // 기본값 md
+}
+
+export default function SettingBtn({
+  onClick,
+  size = "md",
+}: SettingBtnProps) {
+
+  const currentSize = `${sizeStyles[size]}`;
+
+  return (
+    <button
+      className={`${currentSize} relative`}
+      onClick={onClick}
+      aria-label="설정 버튼"
+    >
+      <Image src="/icons/setting-ico.svg" alt="setting button" fill />
+    </button>
+  );
+}
---
+++ src/stories/SettingButton.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import SettingBtn from "../component/iconButton/settingButton/SettingBtn";
+
+const meta: Meta<typeof SettingBtn> = {
+  title: 'Components/IconButton/SettingButton',
+  component: SettingBtn,
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: { type: "radio" },
+      options: ["sm", "md", "lg"],
+      description: '버튼의 크기 (Large, Medium, Small)',
+    },
+    onClick: { 
+        action: 'clicked',
+        description: '버튼 클릭 시 실행될 핸들러',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SettingBtn>;
+
+export const Small: Story = {
+  args: {
+    size: "sm",
+    onClick: () => console.log('SettinButton small Clicked'),
+  },
+  name: 'SettinButton / Small',
+};
+
+export const Medium: Story = {
+  args: {
+    size: "md",
+    onClick: () => console.log('SettinButton Medium Clicked'),
+  },
+  name: 'SettinButton / Medium',
+};
+
+export const Large: Story = {
+  args: {
+    size: "lg",
+    onClick: () => console.log('SettinButton Large Clicked'),
+  },
+  name: 'SettinButton / Large',
+};
