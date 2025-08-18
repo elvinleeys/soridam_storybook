d81615d1a81f689a90b88cc6b884ce910c69850b
Elvin-lee <herjun802@gmail.com>
2025-08-18 23:17:52 +0900

    [Feat]: SearchBar 컴포넌트 및 스토리 완성

---
+++ src/component/searchBar/SearchBar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { flexRow } from "@/mixin/style";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+
+type SearchBarProps = {
+  value: string; // 외부에서 관리하는 입력값
+  onChange: (value: string) => void; // 입력 이벤트 핸들러
+  placeholder?: string;
+};
+
+export default function SearchBar({
+  value,
+  onChange,
+  placeholder = "지번, 도로명 주소 검색",
+}: SearchBarProps) {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
+    };
+
+    return(
+        <div 
+            className={`
+                w-[16.25rem]
+                h-[2.5rem]
+                py-2
+                px-3
+                ${flexRow}
+                items-center
+                gap-[0.5rem]
+                bg-[#F5F5F5]
+                rounded-[6.25rem] 
+                border border-[#808080]
+            `}
+        >
+            <div className="w-[1.5rem] h-[1.5rem] relative">
+                <Image src="/icons/search-ico.svg" alt="search-icon" fill />
+            </div>
+            <input 
+                type="search" 
+                value={value}
+                onChange={handleChange}
+                placeholder={placeholder}
+                className="
+                    flex-1
+                    font-regular
+                    text-base 
+                    placeholder-[#808080]
+                    bg-transparent
+                    focus:outline-none
+                "
+            />
+        </div>
+    );
+}
---
+++ src/stories/SearchBar.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import SearchBar from "../component/searchBar/SearchBar";
+
+const meta: Meta<typeof SearchBar> = {
+  title: "Components/SearchBar",
+  component: SearchBar,
+  tags: ["autodocs"],
+  argTypes: {
+    value: { 
+      control: "text", 
+      description: "검색창의 입력 값(value), 외부 상태와 연동하여 Controlled Input으로 사용" 
+    },
+    onChange: { 
+      action: "changed", 
+      description: "입력값이 변경될 때 호출되는 이벤트 핸들러(onChange)" 
+    },
+    placeholder: { 
+      control: "text", 
+      description: "검색창에 표시되는 안내 문구(placeholder)" 
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SearchBar>;
+
+export const Default: Story = {
+  args: {
+    value: "",
+    placeholder: "지번, 도로명 주소 검색",
+  },
+};
+
+export const WithCustomPlaceholder: Story = {
+  args: {
+    value: "",
+    placeholder: "장소 또는 건물명을 입력하세요",
+  },
+};
