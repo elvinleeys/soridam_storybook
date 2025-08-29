31b61afe8f7f0faab00d87ddf10ca0775699a719
Elvin-lee <herjun802@gmail.com>
2025-08-29 20:45:17 +0900

    [Modified]: Input 컴포넌트 ref 추가 Input 컴포넌트의 많은 재사용으로 forward Ref를 통해 ref를 추가

--- src/design-system/atom/input/Input.tsx
+++ src/design-system/atom/input/Input.tsx
@@ -1,59 +1,69 @@
 "use client";
 
+import { forwardRef } from "react";
 import { motion } from "framer-motion";
 
 type InputProps = {
-    inputType: string;
-    value?: string;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    placeholder?: string;
-    hasError?: boolean;
-};
+  inputType?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  hasError?: boolean;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-export default function Input({
-    inputType="text", 
-    value, 
-    onChange, 
-    placeholder,
-    hasError = false,
-    ...props
-} : InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      inputType = "text",
+      value,
+      onChange,
+      placeholder,
+      hasError = false,
+      ...props
+    },
+    ref
+  ) => {
     return (
-        <motion.div
-            // 흔들림 애니메이션
-            animate={
-                hasError
-                ? {
-                    x: [0, -8, 8, -6, 6, -4, 4, 0],
-                    }
-                : {}
-            }
-            transition={{ duration: 0.4 }}
-            className={`
-                w-full 
-                h-[2.75rem]
-                py-[0.625rem]
-                px-[0.75rem]
-                rounded-[0.5rem]
-                bg-[#F5F5F5]
-                ${hasError ? "border border-[#FF3131]" : "border-none"}
-            `}
-        >
-            <input 
-                type={inputType}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-                {...props}
-                className="
-                    flex-1
-                    font-regular
-                    text-base 
-                    placeholder-[#808080]
-                    bg-transparent
-                    focus:outline-none
-                "
-            />
-        </motion.div>
+      <motion.div
+        animate={
+          hasError
+            ? {
+                x: [0, -8, 8, -6, 6, -4, 4, 0],
+              }
+            : {}
+        }
+        transition={{ duration: 0.4 }}
+        className={`
+          w-full 
+          h-[2.75rem]
+          py-[0.625rem]
+          px-[0.75rem]
+          rounded-[0.5rem]
+          bg-[#F5F5F5]
+          ${hasError ? "border border-[#FF3131]" : "border-none"}
+        `}
+      >
+        <input
+          ref={ref}
+          type={inputType}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          {...props}
+          className="
+            flex-1
+            font-regular
+            text-base 
+            placeholder-[#808080]
+            bg-transparent
+            focus:outline-none
+          "
+        />
+      </motion.div>
     );
-}
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
